ac39b5e04757f36a3c48e448c85cc5fb3feeefd3
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-05-31 15:11:18 +0600

    Update generate_product_makefiles.py

--- universal7885-common/generate_product_makefiles.py
+++ universal7885-common/generate_product_makefiles.py
@@ -46,9 +46,9 @@ class GenerateMK():
             writeln(mk)
             writeln(mk, f'PRODUCT_NAME := {vendor}_{device}')
             writeln(mk)
-            writeln(mk, f'SYSTEM_OPTIMIZE_JAVA := true')
+            writeln(mk, 'SYSTEM_OPTIMIZE_JAVA := true')
             writeln(mk)
-            writeln(mk, f'SYSTEMUI_OPTIMIZE_JAVA := true')
+            writeln(mk, 'SYSTEMUI_OPTIMIZE_JAVA := true')
             writeln(mk)
             if device in HD_DEVICES:
                 writeln(mk, 'TARGET_BOOT_ANIMATION_RES := 1080')
