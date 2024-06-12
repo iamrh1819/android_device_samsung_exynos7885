3a21f9c77c21cec9352dace6226b3e79820b9731
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-06-12 16:52:30 +0600

    Update vendorsetup.sh

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -32,7 +32,7 @@ mv nfc hardware/samsung
 
 echo 'Cloning Samsung Extras [5/8]'
 # Samsung Extra Interfaces
-#rm -rf hardware/lineage/interfaces
+rm -rf hardware/lineage/interfaces
 #git clone https://github.com/iamrh1819/android_hardware_lineage_interfaces -b lineage-20.0 hardware/lineage/interfaces
 git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_interfaces -b lineage-20 hardware/samsung-ext/interfaces
 
