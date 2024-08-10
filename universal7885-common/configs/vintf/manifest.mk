fa97e15e68e07583b367b32b5c76686be91b69c9
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-08-10 14:58:18 +0600

    universal7885: add DERP FCM

--- universal7885-common/configs/vintf/manifest.mk
+++ universal7885-common/configs/vintf/manifest.mk
@@ -11,7 +11,8 @@ DEVICE_MATRIX_FILE := $(MANIFEST_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(MANIFEST_PATH)/device_framework_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    #vendor/lineage/config/device_framework_matrix.xml
+    vendor/derp/config/device_framework_matrix.xml
     
 
 ODM_MANIFEST_SKUS += NFC
