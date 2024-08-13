f5ade2f9114c2c0ef843f13fa572a072834891c8
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-08-14 00:17:36 +0600

    Update universal7885-common.mk

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -93,14 +93,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     bluetooth.profile.vc.server.enabled=true
 
 # Camera
-TARGET_BOARD_CAMERA_COUNT ?= 3
-ifeq ($(TARGET_BOARD_CAMERA_COUNT), 3)
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service.exynos7885
-else
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service
-endif
+    android.hardware.camera.provider@2.5-service.samsung \
+    libhypervintf \
+    libsensorndkbridge
 
 PRODUCT_PACKAGES += \
     libGrallocWrapper \
