765608fe18911576dfc353c37b9394ed85858f10
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-08-16 16:01:23 +0600

    Update universal7885-common.mk

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -386,17 +386,6 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
-# Preopt SystemUI and Settings
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI \
-    SystemUIGoogle \
-    Settings \
-    SettingsGoogle \
-    NexusLauncherRelease
-
-# Compile SystemUI on device with `speed`.
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.systemuicompilerfilter=speed
 
 # Task profiles
 PRODUCT_COPY_FILES += \
