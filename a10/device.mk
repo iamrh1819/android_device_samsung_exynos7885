b76654b48f062d46f9bd02c72b9a41d9125ec596
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-08-25 23:51:07 +0600

    Update device.mk

--- a10/device.mk
+++ a10/device.mk
@@ -1,7 +1,7 @@
 TARGET_LOCAL_ARCH := arm64
 
 $(call inherit-product, vendor/samsung/a10/a10-vendor.mk)
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service_32
