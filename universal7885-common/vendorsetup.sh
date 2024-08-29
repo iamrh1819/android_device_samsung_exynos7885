2ed82ef31c5e4cdbd03652916bc3cab3df84b5ca
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-08-29 18:23:05 +0600

    Update vendorsetup.sh

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -12,7 +12,7 @@ echo 'Cloning Device Tree [2/8]'
 # Device tree for exynos7885
 rm -rf device/samsung
 
-git clone https://github.com/iamrh1819/android_device_samsung_exynos7885 -b A14-NF device/samsung
+git clone https://github.com/iamrh1819/android_device_samsung_exynos7885 -b A14-TenX device/samsung
 
 
 echo 'Cloning Vendor Trees [3/8]'
