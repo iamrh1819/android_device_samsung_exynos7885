9bfcf2158441815928a47daa77726d8d8efb6465
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-09-02 08:35:21 +0600

    Update universal7885-common.mk

--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -360,8 +360,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.samsung-multihal \
-    libsensorndkbridge \
-    sensors.samsung
+    libsensorndkbridge
 
 # Shims
 PRODUCT_PACKAGES += \
