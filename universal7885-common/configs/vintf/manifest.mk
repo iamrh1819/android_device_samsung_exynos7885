ceb68f458da0b003bf2ef05ade56c5d916e03358
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-09-12 08:37:06 +0600

    Update manifest.mk

--- universal7885-common/configs/vintf/manifest.mk
+++ universal7885-common/configs/vintf/manifest.mk
@@ -11,7 +11,6 @@ DEVICE_MATRIX_FILE := $(MANIFEST_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(MANIFEST_PATH)/device_framework_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/blackiron/config/device_framework_matrix.xml
     
     
 
