55840fd46c976d258cadab4a7576a73da6f90e5b
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-10-16 21:59:42 +0600

    Update device.mk

--- a10/device.mk
+++ a10/device.mk
@@ -2,7 +2,6 @@ TARGET_LOCAL_ARCH := arm64
 
 $(call inherit-product, vendor/samsung/a10/a10-vendor.mk)
 
-PRODUCT_PACKAGES += \
-   android.hardware.sensors@1.0-service
+
 
 include device/samsung/a10/common.mk
