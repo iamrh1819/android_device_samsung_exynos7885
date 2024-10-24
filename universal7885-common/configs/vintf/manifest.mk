e3fef8cff47b45609bbc436969df60c79a9ca2be
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2024-10-24 22:41:04 +0600

    Update manifest.mk

--- universal7885-common/configs/vintf/manifest.mk
+++ universal7885-common/configs/vintf/manifest.mk
@@ -11,7 +11,7 @@ DEVICE_MATRIX_FILE := $(MANIFEST_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(MANIFEST_PATH)/device_framework_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/orion/config/device_framework_matrix.xml
+    vendor/bliss/config/device_framework_matrix.xml
     
 
 ODM_MANIFEST_SKUS += NFC
